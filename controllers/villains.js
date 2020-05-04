b57c9f3d8cc3f7a72ff1bda3a19b7b1f3d4b9780
Andrea Walsh <andreawalsh@Andreas-Air.fios-router.home>
2020-05-03 21:31:03 -0400

    Mostly finished.

--- controllers/villains.js
+++ controllers/villains.js
@@ -1,38 +1,50 @@
 const models = require('../models')
 
 const getAllVillains = async (request, response) => {
-  const villains = await models.villains.findAll(({ attributes: ['name', 'movie', 'slug'] }))
+  try {
+    const villains = await models.villains.findAll(({ attributes: ['name', 'movie', 'slug'] }))
 
-  return response.send(villains)
+    return response.send(villains)
+  } catch (error) {
+    return response.status(500).send('Unable to retrieve villain list, Please try again')
+  }
 }
 
 const getVillainBySlug = async (request, response) => {
-  const { slug } = request.params
+  try {
+    const { slug } = request.params
 
-  const foundVillain = await models.villains.findOne({
-    attributes: ['name', 'movie', 'slug'],
-    where: { slug }
-  })
+    const foundVillain = await models.villains.findOne({
+      attributes: ['name', 'movie', 'slug'],
+      where: { slug }
+    })
 
-  return foundVillain
-    ? response.send(foundVillain)
-    : response.sendStatus(404)
+    return foundVillain
+      ? response.send(foundVillain)
+      : response.sendStatus(404)
+  } catch (error) {
+    return response.status(500).send('Unable to retrieve villain, Please try again')
+  }
 }
 
 const saveNewVillain = async (request, response) => {
-  const {
-    name, movie, slug
-  } = request.body
+  try {
+    const {
+      name, movie, slug
+    } = request.body
 
-  if (!name || !movie || !slug) {
-    return response.status(400).send('Required fields are name, movie, and slug')
+    if (!name || !movie || !slug) {
+      return response.status(400).send('Required fields are name, movie, and slug')
+    }
+
+    const newVillain = await models.villains.create({
+      name, movie, slug
+    })
+
+    return response.status(201).send(newVillain)
+  } catch (error) {
+    return response.status(500).send('Unable to save team, Please try again')
   }
-
-  const newVillain = await models.villains.create({
-    name, movie, slug
-  })
-
-  return response.status(201).send(newVillain)
 }
 
 module.exports = { getAllVillains, getVillainBySlug, saveNewVillain }
